c6b4384a6dc08aa34abe1ca544b2c5bc3f6adb86
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-06-14 15:07:35 +0300

    feat: Add HTML form

---
+++ src/main/java/pixel_academy/thymeleaf_app/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package pixel_academy.thymeleaf_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    // metoda pentru a afisa formularul HTML initial
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+
+}
