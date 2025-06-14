b9aaf674e0559089ae1bb12d1f6de1f7558303e9
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-06-14 14:05:46 +0300

    Initial commit

---
+++ src/main/java/pixel_academy/thymeleaf_app/ThymeleafAppApplication.java
@@ -0,0 +1,13 @@
+package pixel_academy.thymeleaf_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel_academy/thymeleaf_app/ThymeleafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel_academy.thymeleaf_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
